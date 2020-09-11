3efe011bb3ca2165b72b07bff80f3696be971472
Goorly <vlad.pigulevsky@gmail.com>
2020-09-11 13:59:09 +0300

    login btn created

--- frontend/src/components/headerComponent/header.js
+++ frontend/src/components/headerComponent/header.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
 import logo_ from '../../images/logo.png'
 
 class Header extends Component{
- render() {
-  return (
-    <header>
+
+  responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <div className="header">
         <img className="logo" src={logo_} alt="Logo"></img>
-    </header>
-  );
- }
+
+        <GoogleLogin
+          className="login_btn"
+          clientId="1014889555336-pjrd86lt7eq3fqt3pnfnnrujq1o5m6md.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+      </div>
+    );
+  }
 }
 
 export default Header;
