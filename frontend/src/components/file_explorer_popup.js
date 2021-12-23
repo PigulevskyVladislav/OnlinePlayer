c2504a4bc296a4ddb97e345ba620b84054c31ecb
v.pigulevsky <vlad.pigulevsky@gmail.com>
2021-12-23 18:04:19 +0300

    FRONTEND - popup style changed

--- frontend/src/components/file_explorer_popup.js
+++ frontend/src/components/file_explorer_popup.js
@@ -3,7 +3,8 @@ import '../css/file_explorer_popup.css';
 
 class FileExplorerPopup extends Component {
   constructor(props) {
-    super();
+    super(props);
+    this.state = {value: ''};
   }
 
   render() {
@@ -11,7 +12,11 @@ class FileExplorerPopup extends Component {
       <div className="popup-box">
         <div className="box">
           <span className="close-icon" onClick={()=>this.props.handleClose(false)}>x</span>
-            {this.props.content}
+          <label className="form-label">
+            Введите название каталога с музыкой:
+            <input className="form-input" />
+            <input className="form-button" type="button" value="Изменить" />
+          </label>
         </div>
       </div>
     );
